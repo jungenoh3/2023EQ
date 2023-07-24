044bb7b965fd73696049887476aec8f4169e595d
jungeyo3 <jungeyo3@naver.com>
2023-07-25 08:48:31 +0900

    클러스터 마커 추가 + 대피처 api 추가 (.g.dart 추가)

---
+++ eqms_test/lib/Api/Retrofit/RestClient.g.dart
@@ -0,0 +1,151 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'RestClient.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SensorInfo _$SensorInfoFromJson(Map<String, dynamic> json) => SensorInfo(
+      id: json['id'] as int,
+      deviceid: json['deviceid'] as String,
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      address: json['address'] as String,
+      manu_comp: json['manu_comp'] as String,
+      facility: json['facility'] as String?,
+      level: json['level'] as String,
+    );
+
+Map<String, dynamic> _$SensorInfoToJson(SensorInfo instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'deviceid': instance.deviceid,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+      'address': instance.address,
+      'manu_comp': instance.manu_comp,
+      'facility': instance.facility,
+      'level': instance.level,
+    };
+
+Shelter _$ShelterFromJson(Map<String, dynamic> json) => Shelter(
+      id: json['id'] as int,
+      dtl_adres: json['dtl_adres'] as String,
+      xcord: (json['xcord'] as num).toDouble(),
+      ycord: (json['ycord'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$ShelterToJson(Shelter instance) => <String, dynamic>{
+      'id': instance.id,
+      'dtl_adres': instance.dtl_adres,
+      'xcord': instance.xcord,
+      'ycord': instance.ycord,
+    };
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RestClient implements RestClient {
+  _RestClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'http://155.230.118.78:1234/EQMS';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<SensorInfo>> getSensorInformation() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<List<dynamic>>(_setStreamType<List<SensorInfo>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/sensor-info/all',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    var value = _result.data!
+        .map((dynamic i) => SensorInfo.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  @override
+  Future<List<Shelter>> getShelter() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result =
+        await _dio.fetch<List<dynamic>>(_setStreamType<List<Shelter>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/shelter/specific',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    var value = _result.data!
+        .map((dynamic i) => Shelter.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
